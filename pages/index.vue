61a4e033d0007400426b8d2923013caafedb3696
htetaungshine@better.hr <htetaungshine>
2020-01-20 16:30:35 +0630

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <main-menu></main-menu>
+  </div>
+</template>
+
+<script>
+  import MainMenu from '~/components/menu/Main.vue';
+  export default {
+    components: {
+      MainMenu
+    },
+
+    head: {
+      title: 'Dashboard'
+    },
+  }
+</script>
---
+++ pages/users/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-table
+    :data="users"
+    border
+    style="width: 90%; margin-top: 20px">
+    <el-table-column
+      prop="id"
+      label="ID"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="Name"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="email"
+      label="Email">
+    </el-table-column>
+    <el-table-column
+      prop="created_at"
+      label="Created At">
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+  import users from '~/apollo/queries/users'
+
+  export default {
+    apollo: {
+      users: users
+    }
+  }
+
+
+</script>
